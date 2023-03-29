5e2d9917ffffb6da73a3af7ccc3db5134728d585
Aditya <aditya18.mail@gmail.com>
2023-03-29 09:39:46 +0530

    Implementing Cart

---
+++ src/store/cart-slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newitem = action.payload;
+      const existingItem = state.items.find((item) => item.it === item.id);
+
+      if (!existingItem) {
+        state.items.push({
+          itemId: newitem.id,
+          price: newitem.price,
+          quantity: 1,
+          totalPrice: newitem.price,
+          name:newitem.title
+        });
+      }
+      else{
+       existingItem.quantity++;
+       existingItem.totalPrice = existingItem.totalPrice+ newitem.price;
+      }
+    },
+    removeItemFromCart(state, action) {},
+  },
+});
